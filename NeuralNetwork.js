8679fa0e3bf0554088cac89f18ff5271bb555f60
Ben Sommer <32485726+ben-sommer@users.noreply.github.com>
2021-09-23 22:49:23 +0100

    Initial Commit

---
+++ NeuralNetwork.js
@@ -0,0 +1,181 @@
+const NeuralNetwork = class {
+  constructor({ target, population, keepFraction, mutateFraction }) {
+    if (population.length == 0) {
+      throw new Error("Population must have individuals");
+    }
+    if (
+      !population.every(
+        (individual) => individual.length == population[0].length
+      )
+    ) {
+      throw new Error("Population must have equally sized individuals");
+    }
+    this._population = population || [];
+
+    if (target.length == 0) {
+      throw new Error("Target must have genes");
+    }
+    this._target = target || [];
+
+    if (keepFraction + mutateFraction > 1) {
+      throw new Error("Invalid fraction values");
+    }
+    this._keepFraction = keepFraction || 0;
+    this._mutateFraction = mutateFraction || 0;
+  }
+
+  evolve() {
+    const numToKeep = this._getNumToKeep(this._population.length);
+
+    const numToMutate = this._getNumToMutate(this._population.length);
+
+    const fitIndividuals = this._selectFitIndividuals({
+      population: this._population,
+      count: numToKeep,
+      target: this._target,
+    });
+
+    const crosses = this._generateCrossOvers({
+      population: fitIndividuals,
+      count: numToMutate,
+    });
+
+    const randoms = this._generateRandomIndividuals({
+      count: this.populationSize - numToKeep - numToMutate,
+      length: this.individualSize,
+    });
+
+    const newPopulation = [...fitIndividuals, ...crosses, ...randoms];
+
+    this._population = newPopulation;
+  }
+
+  get averageFitness() {
+    const averageFitness = this._calculateFitnessPopulation({
+      population: this._population,
+      target: this._target,
+    });
+
+    return averageFitness;
+  }
+
+  get populationSize() {
+    const populationSize = this._population.length;
+
+    return populationSize;
+  }
+
+  get individualSize() {
+    const individualSize = this._population[0].length;
+
+    return individualSize;
+  }
+
+  get population() {
+    return this._population;
+  }
+
+  _calculateFitnessIndividual({ individual, target }) {
+    if (individual.length != target.length) return 0;
+
+    const fitness =
+      individual.reduce((prev, current, index) => {
+        if (current == target[index]) return (prev || 0) + 1;
+
+        return prev;
+      }) || 0;
+
+    return fitness;
+  }
+
+  _calculateFitnessPopulation({ population, target }) {
+    const averageFitness =
+      population
+        .map((individual) =>
+          this._calculateFitnessIndividual({ individual, target })
+        )
+        .reduce((a, b) => a + b) / population.length;
+
+    return averageFitness;
+  }
+
+  _selectFitIndividuals({ population, count, target }) {
+    const fitnessLevels = population
+      .map((individual) =>
+        this._calculateFitnessIndividual({ individual, target })
+      )
+      .map((fitness, index) => ({ genes: population[index], fitness }))
+      .sort((a, b) => b.fitness - a.fitness)
+      .map((individual) => individual.genes);
+
+    const fitIndividuals = fitnessLevels.slice(0, count);
+
+    return fitIndividuals;
+  }
+
+  _crossover({ individualA, individualB }) {
+    if (individualA.length != individualB.length)
+      return { individualA, individualB };
+
+    const crossoverPoint = Math.floor(Math.random() * individualA.length);
+
+    const newA = [
+      ...individualA.slice(0, crossoverPoint),
+      ...individualB.slice(crossoverPoint),
+    ];
+
+    const newB = [
+      ...individualB.slice(0, crossoverPoint),
+      ...individualA.slice(crossoverPoint),
+    ];
+
+    return { newA, newB };
+  }
+
+  _generateCrossOver(population) {
+    const a = population[Math.floor(Math.random() * population.length)];
+    const b = population[Math.floor(Math.random() * population.length)];
+
+    const cross = this._crossover({ individualA: a, individualB: b });
+
+    return cross.newA;
+  }
+
+  _generateCrossOvers({ population, count }) {
+    const crosses = Array(count)
+      .fill()
+      .map(() => this._generateCrossOver(population));
+
+    return crosses;
+  }
+
+  _generateRandomIndividual(length) {
+    const individual = Array(length)
+      .fill()
+      .map((x) => Math.round(Math.random()));
+
+    return individual;
+  }
+
+  _generateRandomIndividuals({ length, count }) {
+    const randoms = Array(count)
+      .fill()
+      .map(() => this._generateRandomIndividual(length));
+
+    return randoms;
+  }
+
+  _getNumToKeep(size) {
+    const toKeep = Math.floor(size * this._keepFraction);
+
+    return toKeep;
+  }
+
+  _getNumToMutate(size) {
+    const toMutate = Math.floor(size * this._mutateFraction);
+
+    return toMutate;
+  }
+};
+
+module.exports = { NeuralNetwork };
---
+++ index.js
@@ -0,0 +1,42 @@
+// Simulation configuration
+const individualSize = 500; // Number of genes in each individual
+const populationSize = 500; // Number of individuals in the population (remains constant throughout)
+const iterations = 100; // Number of times to evolve the population
+const graphHeight = 15; // Height of the average fitness graph in the console - set to `null` to disable
+
+// NN configuration
+const keepFraction = 0.1; // Fraction of each population to keep (rest is discarded)
+const mutateFraction = 0.9; // Fraction of each population to create from the remaining population via crossover
+// Any remaining gap in the population is filled by random individuals
+// e.g. KF=0.2 MF=0.7 - 20% of the population will be kept and used to generate another 70% of the population via
+// crossover, leaving a gap of 10% which is generated randomly
+
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+const { NeuralNetwork } = require("./NeuralNetwork");
+const asciichart = require("./asciichart");
+
+const startingIndividuals = Array(populationSize)
+  .fill()
+  .map(() =>
+    Array(individualSize)
+      .fill()
+      .map((x) => Math.round(Math.random()))
+  );
+
+const net = new NeuralNetwork({
+  target: Array(individualSize).fill(1),
+  population: startingIndividuals,
+  keepFraction: keepFraction,
+  mutateFraction: mutateFraction,
+});
+
+let fitnessLevels = [];
+
+for (i = 0; i < iterations; i++) {
+  fitnessLevels.push(net.averageFitness / individualSize);
+  net.evolve();
+}
+
+if (graphHeight)
+  console.log(asciichart.plot(fitnessLevels, { height: graphHeight }));
